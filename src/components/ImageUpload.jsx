5e15075e2561fd52875c7b0302a3a150cc6b6946
kellyjeffery <kellyjeffery1@hotmail.com>
2020-05-18 13:28:48 +0100

    started image upload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class ImageUpload extends Component {
 
@@ -12,12 +13,23 @@ class ImageUpload extends Component {
         })
     }
     fileUploadHandler = () => {
+        const fd = new FormData
+        fd.append('image', this.state.selectedFiles, this.state.selectedFiles.name);
+        axios.post('', fd, {
+            onUploadProgress: ProgressEvent => {
+ console.log('upload in progress:' + Math.round(progressEvent.loaded / progressEvent.total * 100) + "%")
+            }
+        })
+            .then(res => {
+            console.log(res)
+        })
     
     }
     render() { 
         return ( 
             <div>
-                <input type="file" onChange={this.fileSelectedHandler} />
+                <input style={{ display: 'none' }} type="file" onChange={this.fileSelectedHandler} ref={fileInput => this.fileInput = fileInput} />
+                <button onClick={()=>this.fileInput.click()}>pick file</button>
                 <button onClick={this.fileUploadHandler} >upload</button>
 
         </div>
